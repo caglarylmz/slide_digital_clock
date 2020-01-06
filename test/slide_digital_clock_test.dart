f91e1496ad3bdfda41148abb37224a23aeffa50d
caglarylmz <caglar.ylmz@outlook.com>
2020-01-06 15:24:15 +0300

    test

--- test/slide_digital_clock_test.dart
+++ test/slide_digital_clock_test.dart
@@ -1,5 +1,12 @@
 
-void main() {
+import 'package:flutter_test/flutter_test.dart';
+import 'package:slide_digital_clock/slide_digital_clock.dart';
 
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(DigitalClock());
+
+  });
 }
 
